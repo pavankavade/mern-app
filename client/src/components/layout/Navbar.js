4aaf5127b1493bdc17cc98f7fadbe054e04f5ecd
Pavan Kavade <pavan1kavade@gmail.com>
2019-02-23 21:41:35 +0530

    bought back redux devtools and fixed it

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -64,7 +64,7 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4 ">
         <div className="container">
           <Link className="navbar-brand" to="/">
             WorkStation
@@ -82,8 +82,8 @@ class Navbar extends Component {
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                 <Link className="nav-link" to="/profiles">
-                  {' '}
-                  Workers
+                  {'   '}
+                  &nbsp;&nbsp;&nbsp;Workers
                 </Link>
               </li>
 
@@ -92,7 +92,7 @@ class Navbar extends Component {
           </div>
 
         </div>
-        <div >
+        <div>
           {isAuthenticated ? authLinks : guestLinks}
         </div>
       </nav >
